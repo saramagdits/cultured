2c9203f075cca5c1567e84b06d221fd0d5081dbf
Sara Magdits <saramagdits@gmail.com>
2019-01-08 17:24:12 -0500

    PostgreSQL database connection configuration

---
+++ server/environment/environment.js
@@ -0,0 +1,10 @@
+const environment = {
+  // database connection
+  db: {
+    user: 'cultured_admin',
+    host: 'localhost',
+    database: 'cultured_db',
+    password: 'Hypericum17!'
+  }
+};
+module.exports = environment;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const env = require('./environment/environment');
 
-var app = express();
+const { Pool, Client } = require('pg');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,6 +18,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // TODO consider updating location of static files to shared folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// ===========================
+// DATABASE CONNECTION
+// ===========================
+// a pool connection creates a bunch of connections initially
+// const pool = new Pool(env.db);
+//
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// });
+
+const client = new Client(env.db);
+client.connect();
+// test to add rows
+const query = {
+  text: 'INSERT INTO test(first_name, last_name) VALUES($1, $2)',
+  values: ['greg', 'magdits']
+};
+client.query(query)
+  .then(res => console.log(res.rows[0]))
+  .catch(e => console.error(e.stack));
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res);
+//   client.end();
+// });
+// ===========================
+// ROUTER
+// ===========================
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

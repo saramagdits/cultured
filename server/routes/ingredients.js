03f95343f03b2cfe790087cea7f025fd4eb95ab3
Sara Magdits <saramagdits@gmail.com>
2019-01-14 11:34:22 -0500

    Dropped image_path from the ingredients table.

--- server/routes/ingredients.js
+++ server/routes/ingredients.js
@@ -23,17 +23,16 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   const username = req.body.username;
   const dateCreated = new Date();
-  const avatarPath = '/shared/images/avatar/12345.png';
   const query = {
-    text: 'INSERT INTO users(username, date_created, avatar_path) VALUES($1, $2, $3)',
-    values: [username, dateCreated, avatarPath]
+    text: 'INSERT INTO users(username, date_created) VALUES($1, $2)',
+    values: [username, dateCreated]
   };
   const { data } = await db.query(query)
   // .then(res => console.log(res.rows[0]))
     .then(res => res)
     .catch(e => console.error(e.stack));
   console.log(data);
-  res.send(`You created a user with the username ${username}`);
+  res.send(`You created an ingredient with the name ${username}`);
 });
 
 // export our router to be mounted by the parent application

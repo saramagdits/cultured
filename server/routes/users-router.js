368ce9d95e7126da73f5fde774c833f745a1d3ee
Sara Magdits <saramagdits@gmail.com>
2019-01-16 12:57:08 -0500

    /users and /users/:id routes are working. each returns the data/updated data to the client.

--- server/routes/users-router.js
+++ server/routes/users-router.js
@@ -6,20 +6,45 @@ const users = require('../services/users-service');
 // it allows you to use async functions as route handlers
 const router = new Router();
 
-/* GET users listing. */
+// ===========================
+// /users routes
+// ===========================
+/* GET all users data */
 router.get('/', async (req, res, next) => {
-  const data = await users.getUsers();
+  const data = await users.getAllUsers();
   res.send(data);
 });
 
-/* CREATE a new user */
+/* CREATE a new user and return all user data*/
 router.post('/', async (req, res, next) => {
   // TODO must accept an image to be parsed by multi-part form parser (multer?), which should return the path to be inserted as avatarPath
   // TODO consider assigning a default user avatar here, to be replaced later only if one was chosen
-  const queryValues = {username: req.body.username, dateCreated: new Date(), avatarPath: '/shared/images/avatar/default.png'};
+  const queryValues = {
+    username: req.body.username,
+    dateCreated: new Date(),
+    avatarPath: '/shared/images/avatar/default.png'
+  };
 
   const data = await users.createUser(queryValues);
-  res.send(`You created a user with the username ${queryValues.username}`);
+  res.send(data);
+});
+
+// ===========================
+// /users/:id routes
+// ===========================
+/* GET user listing by id */
+router.get('/:id', async (req, res, next) => {
+  const data = await users.getSingleUser(req.params.id);
+  res.send(data);
+});
+/* UPDATE a user's data and returns it*/
+router.put('/:id', async (req, res, next) => {
+  // TODO check if avatar image has changed here
+  // TODO eventually users should be able to update their password
+  const queryValues = {avatarPath: '/shared/images/avatar/updated.png'};
+
+  const data = await users.updateSingleUser(req.params.id, queryValues);
+  res.send(data);
 });
 
 // export our router to be mounted by the parent application

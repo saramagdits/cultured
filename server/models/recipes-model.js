7ba1bf4f2a66b28c840fd8f75eb7b90684b5b943
Sara Magdits <saramagdits@gmail.com>
2019-01-24 17:06:30 -0500

    Data modeling for multiple results now works

--- server/models/recipes-model.js
+++ server/models/recipes-model.js
@@ -1,7 +1,9 @@
 const RecipesModel = {};
+
+// Accepts a SQL recipe result with multiple rows, and maps it to an object
 RecipesModel.single = (d) => {
-  // Accepts a SQL recipe result with multiple rows, and maps it to an object
   return {
+    id: d[0].id,
     title: d[0].title,
     description: d[0].description,
     imagePath: d[0].image_path,
@@ -17,7 +19,46 @@ RecipesModel.single = (d) => {
         value: row.value,
         quantity: row.quantity,
         unit: row.unit
-      }})
+      }
+    })
   }
+};
+
+RecipesModel.multiple = (d) => {
+  let groupBy = {};
+  d.forEach(row => {
+    if (!groupBy[row.id]) {
+      groupBy[row.id] = [];
+    }
+    groupBy[row.id].push(row);
+  });
+
+  let results = [];
+  Object.keys(groupBy).forEach(id => {
+    let groupByRecords = groupBy[id];
+    results.push(
+      {
+        id: groupByRecords[0].id,
+        title: groupByRecords[0].title,
+        description: groupByRecords[0].description,
+        imagePath: groupByRecords[0].image_path,
+        author: groupByRecords[0].author,
+        authorAvatar: groupByRecords[0].author_avatar,
+        prepTime: groupByRecords[0].prep_time,
+        readyTime: groupByRecords[0].ready_time,
+        difficulty: groupByRecords[0].difficulty,
+        timesFavorited: groupByRecords[0].times_favorited,
+        dateCreated: groupByRecords[0].date_created.toDateString(),
+        ingredients: groupByRecords.map((row) => {
+          return {
+            value: row.value,
+            quantity: row.quantity,
+            unit: row.unit
+          }
+        })
+      }
+    );
+  });
+  return results;
 };
 module.exports = RecipesModel;

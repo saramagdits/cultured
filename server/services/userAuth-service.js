9cf1b1b6e47dc5909936fb8d7be6475b63483cae
Sara Magdits <saramagdits@gmail.com>
2019-01-25 17:03:47 -0500

    cleanup

--- server/services/userAuth-service.js
+++ server/services/userAuth-service.js
@@ -2,8 +2,7 @@ const db = require('../db/userAuth-db');
 const UserAuth = {};
 UserAuth.findUser = async (userInfo) => {
   const userExists = await db.findOneData(userInfo.username);
-  if (userExists) {return userExists}
-  else {return {err: true, user: null}}
+  return (userExists ? userExists : null);
 };
 UserAuth.findUserById = async (userId) => {
   const user = await db.findUserByIdData(userId);

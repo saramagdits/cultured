15b32de39b1bad9f5f95d460320774a42405b202
Sara Magdits <saramagdits@gmail.com>
2019-01-29 15:07:07 -0500

    Update user route now has authentication guard. A user may not update a user's info if their authentication is false or their user id does not match the id path parameter.

--- server/routes/users-router.js
+++ server/routes/users-router.js
@@ -1,5 +1,6 @@
 const Router = require('express-promise-router');
 const users = require('../services/users-service');
+const passport = require('passport');
 
 // create a new express-promise-router
 // this has the same API as the normal express router except
@@ -25,6 +26,7 @@ router.post('/', async (req, res) => {
     dateCreated: new Date(),
     avatarPath: '/assets/images/users/default.png'
   };
+  // TODO check if user already exists before creating it
   const data = await users.createUser(queryValues);
   res.send(data);
 });
@@ -38,14 +40,19 @@ router.get('/:id', async (req, res) => {
   res.send(data);
 });
 /* UPDATE a user's data and returns it*/
-// TODO add authentication here
-router.put('/:id', async (req, res) => {
-  // TODO check if avatar image has changed here
-  // TODO eventually users should be able to update their password
-  const queryValues = {avatarPath: '/assets/images/user/updated.png'};
+router.put('/:id', passport.authenticate('basic', {session: false}), async (req, res) => {
+  // Check if user exists and user id matches
+  if (req.user && req.params.id === req.user.id.toString()) {
+    // TODO check if avatar image has changed here
+    // TODO eventually users should be able to update their password
+    const queryValues = {avatarPath: '/assets/images/user/updated.png'};
 
-  const data = await users.updateSingleUser(req.params.id, queryValues);
-  res.send(data);
+    const data = await users.updateSingleUser(req.params.id, queryValues);
+    res.send(data);
+  } else {
+    res.sendStatus(401)
+  }
+
 });
 
 // export our router to be mounted by the parent application

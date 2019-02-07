e680f08f5b5b23d46076dacd1623db06c222c7f3
Sara Magdits <saramagdits@gmail.com>
2019-02-07 18:59:37 -0500

    Added apiUrl to environment config. All recipes service routes are now working.

--- client/src/app/recipes/recipes.component.ts
+++ client/src/app/recipes/recipes.component.ts
@@ -9,6 +9,7 @@ import {RecipesService} from './recipes.service';
 export class RecipesComponent implements OnInit {
   recipes;
   error;
+
   constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
@@ -17,6 +18,41 @@ export class RecipesComponent implements OnInit {
     // This will return an observable
     this.recipesService.getAllRecipes()
       // .subscribe((data) => { this.recipes = {data}; });
-      .subscribe((data) => { console.log(data); }, error => { console.log(error); });
+      .subscribe((data) => { console.log(data); }, (error) => { console.log(error); });
+  }
+  onGetRecipeById() {
+    this.recipesService.getRecipeById(100)
+      .subscribe((data) => { console.log(data); }, (error) => { console.log(error); });
+  }
+  onSearchRecipesByTitle() {
+    this.recipesService.searchRecipesByTitle('kimchi+pickles')
+      .subscribe((data) => { console.log(data); }, (error) => { console.log(error); });
+  }
+  onSearchRecipesByIngredients() {
+    this.recipesService.searchRecipesByIngredients('salt+cabbage')
+      .subscribe((data) => { console.log(data); }, (error) => { console.log(error); });
+  }
+  onCreateNewRecipe() {
+    const recipe = {
+      title: 'kombucha',
+      description: 'fizzy and fun',
+      difficulty: 'medium',
+      prepTime: '30 minutes',
+      readyTime: '2 weeks',
+      ingredients: [
+        {
+          value: 'water',
+          unit: 'gallon',
+          quantity: 1
+        },
+        {
+          value: 'tea',
+          unit: 'packets',
+          quantity: 5
+        }
+      ]
+    };
+    this.recipesService.createNewRecipe(recipe)
+      .subscribe((data) => { console.log(data); }, (error) => { console.log(error); });
   }
 }

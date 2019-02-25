7128b79c5896c9f5b49167f81588b2237aab9688
Sara Magdits <saramagdits@gmail.com>
2019-02-25 14:41:16 -0500

    Added data binding to recipes-grid. Altered the grid cards to match the recipe-cards. Recipes grid takes the recipe Input data and maps it to an array of cards, with custom row widths based on desktop or handset views. Includes alternating "tall" cards.

--- client/src/app/recipes/recipes-grid/recipes-grid.component.ts
+++ client/src/app/recipes/recipes-grid/recipes-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
@@ -7,27 +7,86 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   templateUrl: './recipes-grid.component.html',
   styleUrls: ['./recipes-grid.component.css']
 })
-export class RecipesGridComponent {
+export class RecipesGridComponent implements OnInit, OnChanges{
+  @Input() recipes;
+  cards;
+  mapCards(recipes) {
+    /** Based on the screen size, switch from standard to one column per row */
+    this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+      map(({ matches }) => {
+        if (matches) {
+          return this.recipes.map(
+            recipe => {
+              recipe.cols = 1;
+              recipe.rows = 1;
+              return recipe;
+            }
+          );
+        }
+        return this.recipes.map(
+          (recipe, index) => {
+            if (index === 0 ) { recipe.cols = 3; recipe.rows = 1; return recipe; }
+            if (index % 7 === 0 ) { recipe.cols = 1; recipe.rows = 2; return recipe; }
+            recipe.cols = 1;
+            recipe.rows = 1;
+            return recipe;
+          }
+        );
+      })
+    );
+  }
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     if (matches) {
+  //       return this.recipes.map(
+  //         recipe => {
+  //           recipe.cols = 1;
+  //           recipe.rows = 1;
+  //           return recipe;
+  //         }
+  //       );
+  //     }
+  //     return this.recipes.map(
+  //       (recipe, index) => {
+  //         if (index === 0 ) { recipe.cols = 3; recipe.rows = 1; return recipe; }
+  //         if (index % 7 === 0 ) { recipe.cols = 1; recipe.rows = 2; return recipe; }
+  //         recipe.cols = 1;
+  //         recipe.rows = 1;
+  //         return recipe;
+  //       }
+  //     );
+  //   })
+  // );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnChanges() {
+    this.mapCards(this.recipes);
+  }
+  ngOnInit() {
+    this.mapCards(this.recipes);
+    /** Based on the screen size, switch from standard to one column per row */
+    // this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    //   map(({ matches }) => {
+    //     if (matches) {
+    //       return this.recipes.map(
+    //         recipe => {
+    //           recipe.cols = 1;
+    //           recipe.rows = 1;
+    //           return recipe;
+    //         }
+    //       );
+    //     }
+    //     return this.recipes.map(
+    //       (recipe, index) => {
+    //         if (index === 0 ) { recipe.cols = 3; recipe.rows = 1; return recipe; }
+    //         if (index % 7 === 0 ) { recipe.cols = 1; recipe.rows = 2; return recipe; }
+    //         recipe.cols = 1;
+    //         recipe.rows = 1;
+    //         return recipe;
+    //       }
+    //     );
+    //   })
+    // );
+  }
 }
